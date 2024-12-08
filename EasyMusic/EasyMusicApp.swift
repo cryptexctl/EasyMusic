a68526811f69e033cc7ab6db3e98d2475cce91de
wheelchairy <d3vkostya@icloud.com>
2024-12-08 12:22:55 +0300

    Initial Commit

---
+++ EasyMusic/EasyMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMusicApp.swift
+//  EasyMusic
+//
+//  Created by Platon on 08.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct EasyMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EasyMusicTests/EasyMusicTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyMusicTests.swift
+//  EasyMusicTests
+//
+//  Created by Platon on 08.12.2024.
+//
+
+import Testing
+@testable import EasyMusic
+
+struct EasyMusicTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
